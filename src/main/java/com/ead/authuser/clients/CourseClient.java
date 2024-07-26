6076f20c86328436bdbcea0362fa863a8eb398d2
Gedione Souza <gedionesouza2003@gmail>
2024-07-26 12:32:25 -0300

    refactor: update version spring security

--- src/main/java/com/ead/authuser/clients/CourseClient.java
+++ src/main/java/com/ead/authuser/clients/CourseClient.java
@@ -38,30 +38,30 @@ public class CourseClient {
     String REQUEST_URL_COURSE;
 
 //    @Retry(name = "retryInstance", fallbackMethod = "retryfallback")
-    @CircuitBreaker(name = "circuitbreakerInstance", fallbackMethod = "circuitbreakerfallback")
-    public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable, String token){
-        List<CourseDto> searchResult = null;
-        String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", token);
-        HttpEntity<String> requestEntity = new HttpEntity<String>("parameters", headers);
-        log.debug("Request URL: {} ", url);
-        log.info("Request URL: {} ", url);
-        ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
-        ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
-        searchResult = result.getBody().getContent();
-        log.debug("Response Number of Elements: {} ", searchResult.size());
-        log.info("Ending request /courses userId {} ", userId);
-        return new PageImpl<>(searchResult);
-    }
+@CircuitBreaker(name = "circuitbreakerInstance")
+public Page<CourseDto> getAllCoursesByUser(UUID userId, Pageable pageable, String token){
+    List<CourseDto> searchResult = null;
+    String url = REQUEST_URL_COURSE + utilsService.createUrlGetAllCoursesByUser(userId, pageable);
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", token);
+    HttpEntity<String> requestEntity = new HttpEntity<String>("parameters", headers);
+    log.debug("Request URL: {} ", url);
+    log.info("Request URL: {} ", url);
+    ParameterizedTypeReference<ResponsePageDto<CourseDto>> responseType = new ParameterizedTypeReference<ResponsePageDto<CourseDto>>() {};
+    ResponseEntity<ResponsePageDto<CourseDto>> result = restTemplate.exchange(url, HttpMethod.GET, requestEntity, responseType);
+    searchResult = result.getBody().getContent();
+    log.debug("Response Number of Elements: {} ", searchResult.size());
+    log.info("Ending request /courses userId {} ", userId);
+    return new PageImpl<>(searchResult);
+}
 
-    public  Page<CourseDto> circuitbreakerfallback(UUID userId, Pageable pageable, Throwable t) {
+    public Page<CourseDto> circuitbreakerfallback(UUID userId, Pageable pageable, Throwable t) {
         log.error("Inside circuit breaker fallback, cause - {}", t.toString());
         List<CourseDto> searchResult = new ArrayList<>();
         return new PageImpl<>(searchResult);
     }
 
-    public  Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
+    public Page<CourseDto> retryfallback(UUID userId, Pageable pageable, Throwable t) {
         log.error("Inside retry retryfallback, cause - {}", t.toString());
         List<CourseDto> searchResult = new ArrayList<>();
         return new PageImpl<>(searchResult);
